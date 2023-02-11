a3bd882b5b6eaf18e617446169c631e011222d7e
Nazila <azabdaftar@outlook.com>
2023-02-11 17:14:27 +0100

    sort has problem!!!But other is finished

--- js/js.js
+++ js/js.js
@@ -1,9 +1,18 @@
-const taskList = JSON.parse(jsonObj);
-//console.log(taskList[11].importance);
-let sortArray = [];
+document.getElementById("btnsPrio").addEventListener("click", function() {
+    const list = document.getElementById("result");
+    list.innerHTML = '';
 
-for (let taskListValue of taskList) {
-    document.getElementById("result").innerHTML += `
+    let result = JSON.stringify(sortArray);
+    let final = JSON.parse(result);
+
+    const sortFinalArray = final.sort(
+        (A, B) => B.importance - A.importance,
+    );
+
+
+    for (let sortValue of sortFinalArray) {
+
+        document.getElementById("result").innerHTML += `
     <div>
 <div class="card shadow-lg h-100">
 <div class="topCard">
@@ -20,10 +29,10 @@ for (let taskListValue of taskList) {
     </div>
 </div>
 
-<img src="${taskListValue.image}" class="card-img-top h-70" alt="${taskListValue.taskName}">
+<img src="${sortValue.image}" class="card-img-top h-70" alt="${sortValue.taskName}">
 <div class="card-body">
-    <h5 class="card-title">${taskListValue.taskName}</h5>
-    <p class="card-text">${taskListValue.description}</p>
+    <h5 class="card-title">${sortValue.taskName}</h5>
+    <p class="card-text">${sortValue.description}</p>
     <hr>
     <div class="priorityLevel">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
@@ -31,7 +40,7 @@ for (let taskListValue of taskList) {
                 d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
         </svg>
         <p class="mx-2">Priority level:</p>
-        <button type="button" class="btn btn-success mx-2 taskPri btnColor" id="taskPriority" ">${taskListValue.importance}</button>
+        <button type="button" class="btn btn-success mx-2 taskPri btnColor" id="taskPriority" ">${sortValue.importance}</button>
     </div>
     <div class="deadtimeCard">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
@@ -52,37 +61,38 @@ for (let taskListValue of taskList) {
 </div>
 </div>
 </div>`;
+        sortArray.push(taskListValue);
+    }
 
-    sortArray.push(taskListValue);
-
-}
-
-let taskPri = document.getElementsByClassName("taskPri");
+    let taskPri = document.getElementsByClassName("taskPri");
 
 
-for (let i = 0; i < taskPri.length; i++) {
 
-    taskPri[i].addEventListener("click", function() {
+    for (let i = 0; i < taskPri.length; i++) {
 
-        taskList[i].importance = taskList[i].importance + 1;
+        taskPri[i].addEventListener("click", function() {
 
-        if (taskList[i].importance <= 5) {
-            document.getElementsByClassName("taskPri")[i].innerHTML =
-                taskList[i].importance;
+            taskList[i].importance = taskList[i].importance + 1;
 
-            if (taskList[i].importance <= 1) {
-                document.getElementsByClassName("taskPri")[i].classList.add("btn-success");
+            if (taskList[i].importance <= 5) {
+                document.getElementsByClassName("taskPri")[i].innerHTML =
+                    taskList[i].importance;
 
-            } else if (taskList[i].importance <= 3) {
-                document.getElementsByClassName("taskPri")[i].classList.remove("btn-success");
-                document.getElementsByClassName("taskPri")[i].classList.add("btn-warning");
+                if (taskList[i].importance <= 1) {
+                    document.getElementsByClassName("taskPri")[i].classList.add("btn-success");
 
-            } else {
-                document.getElementsByClassName("taskPri")[i].classList.remove("btn-warning");
-                document.getElementsByClassName("taskPri")[i].classList.add("btn-danger");
+                } else if (taskList[i].importance <= 3) {
+                    document.getElementsByClassName("taskPri")[i].classList.remove("btn-success");
+                    document.getElementsByClassName("taskPri")[i].classList.add("btn-warning");
+
+                } else {
+                    document.getElementsByClassName("taskPri")[i].classList.remove("btn-warning");
+                    document.getElementsByClassName("taskPri")[i].classList.add("btn-danger");
+
+                }
 
             }
+        })
+    }
 
-        }
-    })
-}
+})
---
+++ js/sortJson.js
@@ -0,0 +1,61 @@
+const jsonSortObj = `[{
+    "taskName": "Hit the gym",
+    "image": "../images/Gym.jpg",
+    "description": "Überall dieselbe alte Leier. Das Layout ist fertig, der Text lässt auf",
+    "importance": 0
+  }, {
+    "taskName": "Pay bills",
+    "image": "../images/PayBills.jpg",
+    "description": "Überall dieselbe alte Leier. Das Layout ist fertig",
+    "importance": 0
+  }, {
+    "taskName": "Meet George",
+    "image": "../images/MeetGeorg.jpg",
+    "description": "Reserve a Restaurant for our Meeting!",
+    "importance": 0
+  }, {
+    "taskName": "Buy eggs",
+    "image": "../images/eggs.jpg",
+    "description": "Shop at Billa.",
+    "importance": 0
+  }, {
+    "taskName": "Read a book",
+    "image": "../Images/books.jpg",
+    "description": "zum Beispiel",
+    "importance": 0
+  }, {
+    "taskName": "Organize office",
+    "image": "../images/organize.jpg",
+    "description": "das es ebenso verdient wahrgenommen zu werden: Webstandards nämlich",
+    "importance": 0
+  }, {
+    "taskName": "Renew car insurance",
+    "image": "../Images/car.jpg",
+    "description": "Denn esse est percipi - Sein ist wahrgenommen",
+    "importance": 0
+  }, {
+    "taskName": "Sign up for online course",
+    "image": "../images/online.jpg",
+    "description": "HTML, CSS, JavaScript",
+    "importance": 0
+  }, {
+    "taskName": "Call Sam For payments",
+    "image": "Images/phone.jpg",
+    "description": "Damit das Layout nun nicht nackt im Raume steht und sich klein und leer vorkommt",
+    "importance": 0
+  }, {
+    "taskName": "Make payment to Bluedart",
+    "image": "../Images/puzzle-2500328_1280.jpg",
+    "description": "ere Sätze lang zu begleiten",
+    "importance": 0
+  }, {
+    "taskName": "Having a Party",
+    "image": "../images/party.jpg",
+    "description": "t percipi - Sein ist wahrgenommen werden. Und weil Sie nun schon die Güte haben, mich ein pa",
+    "importance": 0
+  }, {
+    "taskName": "Send Email to Hannah",
+    "image": "../images/Email.jpg",
+    "description": " Zwecke erschaffen, immer im Schatten meines großen Bruders »Lorem Ipsum",
+    "importance": 0
+  }]`;
