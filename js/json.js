617ea58e80984c9eae87d755db16346a310508c6
Nazila <azabdaftar@outlook.com>
2023-04-28 18:20:48 +0200

    changed Images Path

--- js/json.js
+++ js/json.js
@@ -1,61 +1,61 @@
 const jsonObj = `[{
     "taskName": "Hit the gym",
-    "image": "../images/Gym.jpg",
+    "image": "/images/Gym.jpg",
     "description": "Überall dieselbe alte Leier. Das Layout ist fertig, der Text lässt auf",
     "importance": 0
   }, {
     "taskName": "Pay bills",
-    "image": "../images/PayBills.jpg",
+    "image": "/images/PayBills.jpg",
     "description": "Überall dieselbe alte Leier. Das Layout ist fertig",
     "importance": 0
   }, {
     "taskName": "Meet George",
-    "image": "../images/MeetGeorg.jpg",
+    "image": "/images/MeetGeorg.jpg",
     "description": "Reserve a Restaurant for our Meeting!",
     "importance": 0
   }, {
     "taskName": "Buy eggs",
-    "image": "../images/eggs.jpg",
+    "image": "/images/eggs.jpg",
     "description": "Shop at Billa.",
     "importance": 0
   }, {
     "taskName": "Read a book",
-    "image": "../Images/books.jpg",
+    "image": "/Images/books.jpg",
     "description": "zum Beispiel",
     "importance": 0
   }, {
     "taskName": "Organize office",
-    "image": "../images/organize.jpg",
+    "image": "/images/organize.jpg",
     "description": "das es ebenso verdient wahrgenommen zu werden: Webstandards nämlich",
     "importance": 0
   }, {
     "taskName": "Renew car insurance",
-    "image": "../Images/car.jpg",
+    "image": "/Images/car.jpg",
     "description": "Denn esse est percipi - Sein ist wahrgenommen",
     "importance": 0
   }, {
     "taskName": "Sign up for online course",
-    "image": "../images/online.jpg",
+    "image": "/images/online.jpg",
     "description": "HTML, CSS, JavaScript",
     "importance": 0
   }, {
     "taskName": "Call Sam For payments",
-    "image": "Images/phone.jpg",
+    "image": "images/phone.jpg",
     "description": "Damit das Layout nun nicht nackt im Raume steht und sich klein und leer vorkommt",
     "importance": 0
   }, {
     "taskName": "Make payment to Bluedart",
-    "image": "../Images/puzzle-2500328_1280.jpg",
+    "image": "/images/puzzle-2500328_1280.jpg",
     "description": "ere Sätze lang zu begleiten",
     "importance": 0
   }, {
     "taskName": "Having a Party",
-    "image": "../images/party.jpg",
+    "image": "/images/party.jpg",
     "description": "t percipi - Sein ist wahrgenommen werden. Und weil Sie nun schon die Güte haben, mich ein pa",
     "importance": 0
   }, {
     "taskName": "Send Email to Hannah",
-    "image": "../images/Email.jpg",
+    "image": "/images/Email.jpg",
     "description": " Zwecke erschaffen, immer im Schatten meines großen Bruders »Lorem Ipsum",
     "importance": 0
   }]`;
